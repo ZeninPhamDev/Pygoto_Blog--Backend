c9510aa3fe85ea8caa9013231dac94be7b27653f
ZeninPhamDev <zenin3958@gmail.com>
2024-04-20 16:01:39 +0700

    update api

--- Pygoto_Blog_Api/urls.py
+++ Pygoto_Blog_Api/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
-    path('api/', include('api.urls', namespace='api'))
+    path('api/', include('api.urls', namespace='api')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+   
 ]
--- api/views.py
+++ api/views.py
@@ -4,11 +4,47 @@ from blog.models import Post
 
 from .serializers import PostSerializer
 
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly
+
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+    
+
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+""" Concrete View Classes
+#ListAPIView
+Used for read-only endpoints to represent a collection of model instances.
+Provides a get method handler.
+
+#CreateAPIView
+Used for create-only endpoints.
+Provides a post method handler
+
+#RetrieveAPIView
+Used for read-only endpoints to represent a single model instance.
+Provides a get method handler.
+
+#UpdateAPIView
+Used for update-only endpoints for a single model instance.
+Provides put and patch method handlers.
+
+#DestroyAPIView
+Used for delete-only endpoints for a single model instance.
+Provides a delete method handler.
+
+...
+"""
 
